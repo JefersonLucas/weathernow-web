40826889d2581c69420305d51e65e3213e085781
Jeferson Lucas <jeferson.luckas@gmail.com>
2023-09-25 13:28:31 -0300

    feat: consumed data from api and used react markdown

--- src/pages/privacy/index.tsx
+++ src/pages/privacy/index.tsx
@@ -1,7 +1,37 @@
+import { api } from '@/lib/axios'
+import { convertDateToPtBR } from '@/utils/convert-date-to-ptBR'
+import { useEffect, useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+
+interface PrivacyProps {
+	title: string
+	body: string
+	updated_at: string
+}
+
 export function Privacy() {
+	const [privacy, setPrivacy] = useState<PrivacyProps | null>(null)
+
+	useEffect(() => {
+		api
+			.get('/repos/JefersonLucas/weathernow-web/issues/1')
+			.then((response) => setPrivacy(response.data))
+	}, [])
+
+	if (!privacy) return null
+
 	return (
-		<div className="bg-gray-900 h-screen w-full flex items-center justify-center flex-col gap-6">
-			<h1 className="text-3xl text-white">Privacy</h1>
+		<div className="bg-gray-900 h-full w-full flex items-center justify-center flex-col gap-6 p-8">
+			<h1 className="text-3xl text-white">{privacy.title}</h1>
+
+			<ReactMarkdown
+				children={privacy.body}
+				className="text-white flex gap-2 flex-col"
+			/>
+
+			<span className="text-white">
+				Última Atualização: {convertDateToPtBR(privacy.updated_at)}
+			</span>
 		</div>
 	)
 }
