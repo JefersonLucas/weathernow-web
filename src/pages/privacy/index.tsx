36e5f4bb7edb3677b309fef56eaf6ec96a997391
Jeferson Lucas <jeferson.luckas@gmail.com>
2023-09-25 12:46:15 -0300

    feat: created home and privacy pages

---
+++ src/pages/privacy/index.tsx
@@ -0,0 +1,7 @@
+export function Privacy() {
+	return (
+		<div className="bg-gray-900 h-screen w-full flex items-center justify-center flex-col gap-6">
+			<h1 className="text-3xl text-white">Privacy</h1>
+		</div>
+	)
+}
