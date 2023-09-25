173c9f920939e74b1e35e46f69e8cba104aa6664
Jeferson Lucas <jeferson.luckas@gmail.com>
2023-09-25 12:46:38 -0300

    feat: configured router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import logoFull from './assets/full-logo.svg'
+import { Router } from '@/router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
 	return (
-		<div className="bg-gray-900 h-screen w-full flex items-center justify-center flex-col gap-6">
-			<img src={logoFull} alt="" className="w-[250px]" />
-		</div>
+		<BrowserRouter>
+			<Router />
+		</BrowserRouter>
 	)
 }
